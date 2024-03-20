b4fc1066449b52a6bbc6c5b211f5980a052c2ee6
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-03-19 22:09:00 -0600

    Nuevos cambios
    Nuevos cambios

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "par o impar.cpp"
 using namespace std;
 
-main(){
+
+int main(){
+
+	int num;
+
+	cout<<"Ingrese el Numero de la funcion"<<endl;
+		do{
+			cout<<"2. Determinar Numero Par o Impar: ";
+			cin>>num;
+   		 if(num == 2){
+			cout<<"_____Ejecutando Numeros Par o Impar_______"<<endl;
+	 		par_o_impar();
+	 		return 0;
+		}else {
+	  		cout<<"Ingrese un dato correcto "<<endl; 
+			cout<<"__________________________________________"<<endl;	  
+		}
+	}while (true);
 	
 	
+		
+
 }
