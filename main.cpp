592abe4816ec7d5281e279102798f545a2fc9be5
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-03-25 21:08:54 -0600

    Actualizacion Main
    Actualizacion del Main, agregandole la funcion del puntero pantalla

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "par o impar.cpp"
 #include "Tabla_Multiplicar.cpp"
 #include "Hipotenusa.cpp"
+#include "Puntero_pantalla.cpp"
 using namespace std;
 
 
@@ -12,27 +13,34 @@ int main(){
 
 	cout<<"Ingrese el Numero de la funcion"<<endl;
 		do{
+			cout<<""<<endl;
 			cout<<"2. Determinar Numero Par o Impar: "<<endl;
 				cout<<"8. Tabla de Multiplicar:"<<endl;
-					cout<<"16. Calcular la Hipotenusa:  "<<endl;
-						cout<<"Ingrese 0 para salir "<<endl;
-							cout<<"___________________________________"<<endl;
-								cin>>num;					
+				 	cout<<"14. Puntero en Pantalla:"<<endl;
+						cout<<"16. Calcular la Hipotenusa:  "<<endl;
+							cout<<"Ingrese 0 para salir "<<endl;
+								cout<<""<<endl;
+								cout<<"___________________________________"<<endl;
+									cin>>num;					
 		if(num == 2){
 			cout<<"_____Ejecutando Numeros Par o Impar_______"<<endl;
 	 		par_o_impar();
 				}else if(num == 8){
 					cout<<"_______Ejecutando Tabla de Multiplicar________"<<endl;
 					multiplicar();
-						}else if(num == 16){
+						}else if(num == 14){
+									cout<<"_______Ejecutando puntero en Pantalla_______"<<endl;
+									puntero();
+							}else if(num == 16){
 							cout<<"_______Ejecutando Calculo de Hipotenusar________"<<endl;
 							hipotenusa();
 								}else if(num == 0){
 									cout<<"Saliendo del Programa_______"<<endl;
 									break;
 										}else {
-	  										cout<<"Ingrese un dato correcto "<<endl; 
-											cout<<"__________________Vuelva a Intentarlo__________________"<<endl;	  
+	  										cout<<"Ingrese un dato correcto "<<endl;
+											  cout<<""<<endl; 
+											cout<<"__________Vuelva a Intentarlo__________"<<endl;	  
 	}
 	}while (true);
 	
