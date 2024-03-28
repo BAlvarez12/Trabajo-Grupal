0b10fc0527a111b890d333192ad967839bc074a1
DaryPerez <161182193+DaryPerez@users.noreply.github.com>
2024-03-27 20:53:16 -0600

    Update decimales_binario.cpp

--- decimales_binario.cpp
+++ decimales_binario.cpp
@@ -1,8 +1,21 @@
-/* Conversión de número decimal a número binario */
+/ConversiÃ³n de nÃºmero decimal a nÃºmero binario/
 
-#include <stdio.h>
+#include <iostream>
 
 int main() {	
+	int n, c, k;
+	cout << "Ingrese numero decimal: "<<endl;
+	cin >> "%d",&n;
+
+	cout <<"%d en binario es: " << n << endl;
+
+	for (c=7;c>=0;c--){
+		k = n>>c;
+		if (k&1){
+		cout << "1"<<endl;
+		}else {
+		cout <<"0"<<endl;
+		}
 	
 	return 0;
 }
