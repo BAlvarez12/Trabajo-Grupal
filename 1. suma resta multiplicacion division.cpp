3dbed29a358bb0c0a6e825d79928cc372cd8c468
DaryPerez <161182193+DaryPerez@users.noreply.github.com>
2024-03-27 20:07:55 -0600

    suma_resta_multiplicacion_division

---
+++ 1. suma resta multiplicacion division.cpp
@@ -0,0 +1,21 @@
+//sum, Subtraction, Multiplication and Division of two numbers
+
+#include <iostream>
+using namespace std;
+int main() {
+
+int num1;
+int num2;
+
+cout << "ingrese primer numero " <<endl;
+cin >> num1;
+cout << "ingrese segundo numero " <<endl;
+cin >> num2;
+cout << "suma: " << num1+num2 << endl;
+cout << "resta: " << num1-num2 << endl;
+cout << "Multiplicacion: " << num1*num2 << endl;
+cout << "division: " << num1/num2 << endl;
+
+    return 0;
+}
+
