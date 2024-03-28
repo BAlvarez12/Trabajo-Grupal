a750d83fe52f808743a157e54d8cd3c54e9e8401
DaryPerez <161182193+DaryPerez@users.noreply.github.com>
2024-03-27 20:23:27 -0600

    Update main.cpp
    Se agrega suma resta multiplicacion y division

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Tabla_Multiplicar.cpp"
 #include "Hipotenusa.cpp"
 #include "Puntero_pantalla.cpp"
+#include "sum_res_mul_div"
 using namespace std;
 
 
@@ -14,6 +15,7 @@ int main(){
 	cout<<"Ingrese el Numero de la funcion"<<endl;
 		do{
 			cout<<""<<endl;
+			cout<<"1. Suma, resta, multiplicacion y division: "<<endl;
 			cout<<"2. Determinar Numero Par o Impar: "<<endl;
 				cout<<"8. Tabla de Multiplicar:"<<endl;
 				 	cout<<"14. Puntero en Pantalla:"<<endl;
@@ -22,25 +24,34 @@ int main(){
 								cout<<""<<endl;
 								cout<<"___________________________________"<<endl;
 									cin>>num;					
+		if(num == 1){
+		cout<<"_____Ejecutando suma, resta, multiplicacion y divicion_______"<<endl;
+	 	sum_res_mul_div();
+		}else 
+			
 		if(num == 2){
-			cout<<"_____Ejecutando Numeros Par o Impar_______"<<endl;
-	 		par_o_impar();
-				}else if(num == 8){
-					cout<<"_______Ejecutando Tabla de Multiplicar________"<<endl;
-					multiplicar();
-						}else if(num == 14){
-									cout<<"_______Ejecutando puntero en Pantalla_______"<<endl;
-									puntero();
-							}else if(num == 16){
+		cout<<"_____Ejecutando Numeros Par o Impar_______"<<endl;
+	 	par_o_impar();
+		}else 
+				if(num == 8){
+				cout<<"_______Ejecutando Tabla de Multiplicar________"<<endl;
+				multiplicar();
+				}else 
+						if(num == 14){
+						cout<<"_______Ejecutando puntero en Pantalla_______"<<endl;
+						puntero();
+						}else 
+							if(num == 16){
 							cout<<"_______Ejecutando Calculo de Hipotenusar________"<<endl;
 							hipotenusa();
-								}else if(num == 0){
-									cout<<"Saliendo del Programa_______"<<endl;
-									break;
-										}else {
-	  										cout<<"Ingrese un dato correcto "<<endl;
-											  cout<<""<<endl; 
-											cout<<"__________Vuelva a Intentarlo__________"<<endl;	  
+							}else 
+								if(num == 0){
+								cout<<"Saliendo del Programa_______"<<endl;
+								break;
+									}else {
+	  								cout<<"Ingrese un dato correcto "<<endl;
+									cout<<""<<endl; 
+									cout<<"__________Vuelva a Intentarlo__________"<<endl;	  
 	}
 	}while (true);
 	
