1b029dbd535966472cac4cf46db1be31fa12af78
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-03-27 23:41:36 -0600

    main actualizado
    el main fue actualizado con el Anno bisiesto, convertidor de medidas y enteros a letras

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-#include "par o impar.cpp"
-#include "Tabla_Multiplicar.cpp"
-#include "Hipotenusa.cpp"
-#include "Puntero_pantalla.cpp"
-#include "sum_res_mul_div"
-#include "arabigos_romanos"
-#include "decimales_binario"
-#include "area_triangulo"
+#include "par o impar.cpp" // Bryann
+#include "Tabla_Multiplicar.cpp"// Bryann
+#include "Hipotenusa.cpp" // Bryann
+#include "Puntero_pantalla.cpp"// Bryann
+#include "Anno bisiesto.cpp" //Bryann
+#include "Convertidor de medidas.cpp" // Bryann
+#include "enteros a letras.cpp" // Bryann
+
+// #include "sum_res_mul_div.cpp"// Dary
+// #include "arabigos_romanos.cpp" // Dary
+// #include "decimales_binario.cpp"// Dary
+// #include "area_triangulo.cpp" // Dary
 
 using namespace std;
 int main(){
@@ -14,44 +18,44 @@ int main(){
 	int num;	
 	cout<<"Ingrese el Numero de la funcion"<<endl;
 		do{
+			cout<<"________________________"<<endl;
 			cout<<""<<endl;
 			cout<<"1. Suma, resta, multiplicacion y division: "<<endl;
 			cout<<"2. Determinar Numero Par o Impar: "<<endl;
+			cout<<"3. Convertidor de medidas: "<<endl;
 			cout<<"5. Convertir numeros arabigos a romanos: "<<endl;
+			cout<<"6. Numeros enteros con decimal a letras: "<<endl;
 			cout<<"8. Tabla de Multiplicar:"<<endl;
 			cout<<"11. Conversion de numeros decimales a binarios: "<<endl;
 			cout<<"14. Puntero en Pantalla:"<<endl;
 			cout<<"16. Calcular la Hipotenusa:  "<<endl;
 			cout<<"19. Calcular el area de un triangulo: "<<endl;
+			cout<<"21. Validar anno bisiesto: "<<endl;
 			cout<<"Ingrese 0 para salir "<<endl;
 			cout<<""<<endl;
 			cout<<"___________________________________"<<endl;
 			cin>>num;	
-
-		if(num == 1){
-		cout<<"_____Ejecutando suma, resta, multiplicacion y divicion_______"<<endl;
-	 	sum_res_mul_div();
-		}else 
 			
 		if(num == 2){
 		cout<<"_____Ejecutando Numeros Par o Impar_______"<<endl;
 	 	par_o_impar();
-		}else 
-
-		if(num == 5){
-		cout<<"_____Ejecutando Convertir numero arabigos a romanos_______"<<endl;
-	 	arabigos_romanos();
-		}else 
+		}else  
+		
+		if(num == 3){
+		cout<<"_______Ejecutando Convertidor de medidas________"<<endl;
+		convertidor_medidas();
+		}else  
+		
+		if(num == 6){
+		cout<<"_______Ejecutando Convertidor de numeros a letras________"<<endl;
+		numero_entero();
+		
+		}else  
 		
 		if(num == 8){
 		cout<<"_______Ejecutando Tabla de Multiplicar________"<<endl;
 		multiplicar();
-		}else 
-
-		if(num == 11){
-		cout<<"_______Ejecutando numeros decimales a binarios________"<<endl;
-		decimales_binario();
-		}else 
+		}else  
 		
 		if(num == 14){
 		cout<<"_______Ejecutando puntero en Pantalla_______"<<endl;
@@ -63,16 +67,16 @@ int main(){
 		hipotenusa();
 		}else 
 
-		if(num == 19){
-		cout<<"_______Ejecutando area de un triangulo________"<<endl;
-		area_triangulo();
+		if(num == 21){
+		cout<<"_______Ejecutando verificador de anno bisiesto________"<<endl;
+		anno();
 		}else 
 								
 				if(num == 0){
-				cout<<"Saliendo del Programa_______"<<endl;
+				cout<<"Saliendo del Programa"<<endl;
 				break;
 				}else {
-				cout<<"Ingrese un dato correcto "<<endl;
+				cout<<"_______Ingrese un dato correcto______"<<endl;
 				cout<<""<<endl; 
 				cout<<"__________Vuelva a Intentarlo__________"<<endl;	  
 	}
